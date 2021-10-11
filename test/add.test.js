129ed869c11a94a281c0870fa0fccc9ffa318a9c
vishwajeetgade <vishugade15@gmail.com>
2021-10-11 17:30:33 +0530

    refactor parse Int

--- test/add.test.js
+++ test/add.test.js
@@ -1,8 +1,19 @@
+function getInt(num) {
+    if(num.includes(",")) {
+        let numInt = num.split(",").map(Number);
+        return numInt;
+    }else{
+        return parseInt(num);
+    }
+}
+
 function add(num) {
     if(!num) return 0;
-    else{
-        let numInt = num.split(",");
-        return parseInt(numInt[0])+parseInt(numInt[1]);
+    else if(num.includes(",")){
+        const val = getInt(num);
+        return val[0] + val[1];
+    }else{
+        return getInt(num);
     }
 }
 
@@ -13,6 +24,10 @@ describe("Add Numbers", () => {
         //expect() - what output is , toBe() - what output should be
         expect(add("")).toBe(0);
     });
+    
+    it("return a num", () => {
+        expect(add("1")).toBe(1);
+    })
 
     it("add two number by comma separating", () => {
         expect(add("1,2")).toBe(3);
