57268c52a2ea3ff62babcf547e9c4e946f0f849d
vishwajeetgade <vishugade15@gmail.com>
2021-10-11 17:21:38 +0530

    add two number by comma separation

--- test/add.test.js
+++ test/add.test.js
@@ -1,5 +1,9 @@
 function add(num) {
     if(!num) return 0;
+    else{
+        let numInt = num.split(",");
+        return parseInt(numInt[0])+parseInt(numInt[1]);
+    }
 }
 
 // describe() - to describe the function we are tests
@@ -9,4 +13,8 @@ describe("Add Numbers", () => {
         //expect() - what output is , toBe() - what output should be
         expect(add("")).toBe(0);
     });
+
+    it("add two number by comma separating", () => {
+        expect(add("1,2")).toBe(3);
+    })
 })
