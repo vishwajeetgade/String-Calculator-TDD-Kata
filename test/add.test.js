fab9ab01019ca59b02d97e887c68e7d5d705dac0
vishwajeetgade <vishugade15@gmail.com>
2021-10-11 18:18:17 +0530

    except new line as valid delimiter

--- test/add.test.js
+++ test/add.test.js
@@ -1,17 +1,11 @@
 function getInt(num) {
-    if (num.includes(",")) {
-        let sum = num.split(",").map(i => parseInt(i)).reduce((prev, curr) => prev + curr, 0);
-        return sum;
-    } else {
-        return parseInt(num);
-    }
+    let number = num.replace(/\r?\n/g, ',').split(",").map(i => parseInt(i));
+    return number.reduce((prev, curr) => prev+curr,0 );
 }
 
 function add(num) {
-    if (!num) return 0;
-    else if (num.includes(",")) {
-        return getInt(num);
-    } else {
+    if (!num) return 0; 
+    else {
         return getInt(num);
     }
 }
@@ -33,5 +27,9 @@ describe("Add Numbers", () => {
     })
     it("adding multiple numbers by comma separating", () => {
         expect(add("1,2,3,4")).toBe(10);
+    })
+
+    it("except new line as valid delimiter", () => {
+        expect(add("1,2\n3")).toBe(6);
     })
 })
